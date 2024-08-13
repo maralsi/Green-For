061162e9d6d84ddae79a6b9853588bd419d805b0
maralsi <maral.sagynalieva@gmail.com>
2024-08-13 23:45:47 +0600

    'homework2'

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    rate = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
