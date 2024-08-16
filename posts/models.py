41699677197d301acf92007e48fa14e42c444bd4
maralsi <maral.sagynalieva@gmail.com>
2024-08-16 18:25:03 +0600

    homework3

--- posts/models.py
+++ posts/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class Post(models.Model):
+    image = models.ImageField(null=True, blank=True, upload_to="posts")
     title = models.CharField(max_length=50)
     content = models.TextField(null=True, blank=True)
     rate = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
-
     def __str__(self):
         return self.title
-
